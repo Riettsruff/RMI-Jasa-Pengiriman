5027813b3e99d677d22873784eed3f5630df9ebc
Rietts Andreas Ruff <Riettsruff@gmail.com>
2021-03-07 14:07:39 +0700

    Add Connection Service

---
+++ src/jasa_pengiriman/server/service/Conn.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jasa_pengiriman.server.service;
+
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class Conn {
+  private static Connection conn;
+  
+  public static Connection Conn() {
+    if(conn == null) {
+      try {
+        MysqlDataSource dataSource = new MysqlDataSource();
+
+        dataSource.setServerName("localhost");
+        dataSource.setPort(3306);
+        dataSource.setDatabaseName("db_jasa_pengiriman");
+        dataSource.setUser("root");
+        dataSource.setPassword("");
+
+        conn = (Connection) dataSource.getConnection();
+      } catch(SQLException se){
+        se.printStackTrace();
+      } catch(Exception e){
+        e.printStackTrace();
+      }
+    }
+
+    return conn;
+  }
+}
