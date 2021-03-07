7c5e2a461c6d6b0156080a4dff7e0642084627ab
Rietts Andreas Ruff <Riettsruff@gmail.com>
2021-03-07 14:19:20 +0700

    Initialize DB Helper

--- src/jasa_pengiriman/server/dao/helper/DB.java
+++ src/jasa_pengiriman/server/dao/helper/DB.java
@@ -5,10 +5,33 @@
  */
 package jasa_pengiriman.server.dao.helper;
 
+import static jasa_pengiriman.server.service.Conn.Conn;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  *
  * @author Riett
  */
 public class DB {
   
+  public static ResultSet getAll(String table) {
+    ResultSet rs = null;
+    
+    try {
+      String query = "SELECT * FROM ?";
+
+      PreparedStatement ps = Conn().prepareStatement(query);
+      ps.setString(1, table);
+      rs = ps.executeQuery();
+      
+    } catch (SQLException se) {
+      se.printStackTrace();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    return rs;
+  }
 }
