2d022770a47d998fff4e25e31e67fef2bad54b56
Rietts Andreas Ruff <Riettsruff@gmail.com>
2021-03-16 03:13:35 +0700

    Add RiwayatPeran Model

---
+++ src/jasa_pengiriman/model/RiwayatPeran.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jasa_pengiriman.model;
+
+import java.sql.Timestamp;
+
+/**
+ *
+ * @author Riett
+ */
+public class RiwayatPeran {
+  private int idRiwayatPeran;
+  private Pengguna pengguna;
+  private Peran peran;
+  private Timestamp tanggalDibuat;
+  
+  public void setIdRiwayatPeran(int idRiwayatPeran) {
+    this.idRiwayatPeran = idRiwayatPeran;
+  }
+  
+  public int getIdRiwayatPeran() {
+    return idRiwayatPeran;
+  }
+  
+  public void setPengguna(Pengguna pengguna) {
+    this.pengguna = pengguna;
+  }
+  
+  public Pengguna getPengguna() {
+    return pengguna;
+  }
+  
+  public void setPeran(Peran peran) {
+    this.peran = peran;
+  }
+  
+  public Peran getPeran() {
+    return peran;
+  }
+  
+  public void setTanggalDibuat(Timestamp tanggalDibuat) {
+    this.tanggalDibuat = tanggalDibuat;
+  }
+  
+  public Timestamp getTanggalDibuat() {
+    return tanggalDibuat;
+  }
+}
