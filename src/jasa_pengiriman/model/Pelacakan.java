f567a883a5bbf75a67d6f7c8f02baded179fa7fb
Rietts Andreas Ruff <riettsruff@gmail.com>
2021-03-23 05:50:13 +0700

    Add StatusPelacakan Model

--- src/jasa_pengiriman/model/Pelacakan.java
+++ src/jasa_pengiriman/model/Pelacakan.java
@@ -16,8 +16,9 @@ public class Pelacakan implements Serializable {
   private int idPelacakan;
   private Pengiriman pengiriman;
   private Cabang cabang;
+  private StatusPelacakan statusPelacakan;
   private Timestamp waktuLapor;
-  private String status, keterangan;
+  private String keterangan;
   
   public void setIdPelacakan(int idPelacakan) {
     this.idPelacakan = idPelacakan;
@@ -43,20 +44,20 @@ public class Pelacakan implements Serializable {
     return cabang;
   }
   
+  public StatusPelacakan getStatusPelacakan() {
+    return statusPelacakan;
+  }
+
+  public void setStatusPelacakan(StatusPelacakan statusPelacakan) {
+    this.statusPelacakan = statusPelacakan;
+  }
+  
   public void setWaktuLapor(Timestamp waktuLapor) {
     this.waktuLapor = waktuLapor;
   }
   
   public Timestamp getWaktuLapor() {
     return waktuLapor;
-  }
-  
-  public void setStatus(String status) {
-    this.status = status;
-  }
-  
-  public String getStatus() {
-    return status;
   }
   
   public void setKeterangan(String keterangan) {
---
+++ src/jasa_pengiriman/model/StatusPelacakan.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jasa_pengiriman.model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Riett
+ */
+public class StatusPelacakan implements Serializable {
+  private int idStatusPelacakan;
+  private String namaStatus;
+
+  public int getIdStatusPelacakan() {
+    return idStatusPelacakan;
+  }
+
+  public void setIdStatusPelacakan(int idStatusPelacakan) {
+    this.idStatusPelacakan = idStatusPelacakan;
+  }
+
+  public String getNamaStatus() {
+    return namaStatus;
+  }
+
+  public void setNamaStatus(String namaStatus) {
+    this.namaStatus = namaStatus;
+  }
+  
+  @Override
+  public String toString() {
+    return namaStatus;
+  }
+}
