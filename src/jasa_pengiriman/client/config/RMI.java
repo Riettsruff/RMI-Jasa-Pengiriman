0ee86ab36c601b4f755f67814eaf5c243f41ac25
Rietts Andreas Ruff <riettsruff@gmail.com>
2021-03-23 06:17:17 +0700

    Add RMI Config On Client

---
+++ src/jasa_pengiriman/client/config/RMI.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jasa_pengiriman.client.config;
+
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Riett
+ */
+public class RMI {
+  private static final String HOST = "localhost";
+  private static final String PORT = "3001";
+  
+  public static String getHost() {
+    return HOST;
+  }
+  
+  public static String getPort() {
+    return PORT;
+  }
+  
+  public static Object getService(String service) 
+    throws NotBoundException, MalformedURLException, RemoteException {
+    return Naming.lookup("rmi://" + HOST + ":" + PORT + "/" + service);
+  }
+}
