81f4f03cfa29706b9450310a596452cbfce46b44
Rietts Andreas Ruff <riettsruff@gmail.com>
2021-03-27 19:46:33 +0700

    Add BasicValidation On Client Service

---
+++ src/jasa_pengiriman/client/service/BasicValidation.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jasa_pengiriman.client.service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Riett
+ */
+public class BasicValidation {
+  private static final String EMAIL_REGEX = "^[\\w-_\\.+]*[\\w-_\\.]\\@([\\w]+\\.)+[\\w]+[\\w]$";
+  private static final String NUMERIC_REGEX = "-?\\d+(\\.\\d+)?";
+  
+  public static boolean isValid(HashMap<HashMap<String, Object>, List<String>> data) {
+    for(Map.Entry<HashMap<String, Object>, List<String>> entry : data.entrySet()) {
+      Map.Entry<String, Object> input = entry.getKey().entrySet().iterator().next();
+      List<String> options = entry.getValue();
+      
+      String inputLabel = input.getKey();
+      String inputValue = input.getValue() == null ? "" : input.getValue().toString();
+      
+      for(String o : options) {
+        switch(o) {
+          case "REQUIRED":
+            if(inputValue.isEmpty()) {
+              JOptionPane
+                .showMessageDialog(null, inputLabel + " wajib diisi.", "Oops!", JOptionPane.ERROR_MESSAGE);
+              return false;
+            }
+            break;
+          case "EMAIL":
+            if(!inputValue.matches(EMAIL_REGEX)) {
+              JOptionPane
+                .showMessageDialog(null, "Format " + inputLabel + " tidak sesuai.", "Oops!", JOptionPane.ERROR_MESSAGE);
+              return false;
+            }
+            break;
+          case "NUMERIC":
+            if(!inputValue.matches(NUMERIC_REGEX)) {
+              JOptionPane
+                .showMessageDialog(null, inputLabel + " wajib berupa angka.", "Oops!", JOptionPane.ERROR_MESSAGE);
+              return false;
+            }
+        }
+      }
+    }
+    
+    return true;
+  }
+}
